38b8c5e0800a5dcbe27544f77fbfae5e2c7f4003
Takehiro Adachi <takehiro0740@gmail.com>
2013-01-07 17:00:44 +0900

    Revert "octopress -> jekyll"
    This reverts commit 5103c8e49738cd87ac3e2722dc825bf4994e95a9.

--- README.md
+++ README.md
@@ -1,21 +1,5 @@
-# Takehiro Adachi
+# Takehiro Adachi blog
 
-Copyright (c) 2012 Takehiro Adachi
+powered by  <http://octopress.org>
 
-This is the [Jekyll](http://github.com/mojombo/jekyll) source for my
-[blog](http://takehiro-adachi.github.com). Great thanks to
-[mojombo](https://github.com/mojombo) for
-[Jekyll](http://github.com/mojombo/jekyll).
-
-# License
-
-The following directories and their contents are Copyright Takehiro Adachi.
-You may not reuse anything therein without my permission:
-
-* _posts/
-* assets/image/
-  
-All other directories and files are MIT Licensed. Feel free to use the HTML and
-CSS as you please. If you do use them, a link back to http://takehiro-adachi.github.com would
-be appreciated, but is not required. I would suggest modifying it so that
-we have some variety. :)
+great thanks to [github Co-Founder Tom Preston-Werner](https://github.com/mojombo) for [jekeyll](https://github.com/mojombo/jekyll)
--- config.rb
+++ config.rb
@@ -1,19 +1,16 @@
 # Require any additional compass plugins here.
+project_type = :stand_alone
 
-# Set this to the root of your project when deployed:
+# Publishing paths
 http_path = "/"
-css_dir = "assets/stylesheet/"
-sass_dir = "assets/stylesheet/sass/"
-images_dir = "assets/image/"
-javascripts_dir = "assets/javascript/"
+http_images_path = "/images"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :compressed
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+fonts_dir = "source/fonts"
 
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-
-preferred_syntax = :sass
+line_comments = false
+output_style = :compressed
